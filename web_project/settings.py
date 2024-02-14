a025874a5ca93af0f7958ff22a66a4e1124a5b47
Admin <ellie.otoole7@gmail.com>
2024-02-14 12:28:02 +0000

    add host name

--- web_project/settings.py
+++ web_project/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = [
     #'localhost',
     #'127.0.0.1'
     #'vscode-django-tutorial.azurewebsites.net'  # Sample name only!
+    'djangosessiondemo.onrender.com'
 ]
 
 # Application definition
